b640950763d1d4477f7e0fb08930d51a22b29d98
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-21 00:59:12 +0100

    routes update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+
+import Register from './pages/Register/Register';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+import "./index.css"
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
     </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
