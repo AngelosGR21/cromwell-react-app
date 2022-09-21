4a5cc334e33f04390e1066ad602fb253b0e2852a
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-21 17:25:02 +0100

    adding loginUser function with lifecycle actions

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { createAsyncThunk , createSlice } from "@reduxjs/toolkit";
 
+const url = "http://localhost:5000"
+
 export const registerUser = createAsyncThunk(
     "authentication/registerUser",
     async (data) => {
-            const request = await axios.post("http://localhost:5000/user/register", data)
+            const request = await axios.post(`${url}/user/register`, data)
             return {token: request.headers.authorization, status: request.status}
+    }
+)
+
+export const loginUser = createAsyncThunk(
+    "authentication/loginUser",
+    async (data) => {
+        const request = await axios.post(`${url}/user/login`, data)
+        return {token: request.headers.authorization, status: request.status}
     }
 )
 
@@ -30,8 +40,15 @@ const authSlice = createSlice({
             state.token = action.payload.token
         },
         [registerUser.rejected] : (state, action) => {
-            if(action.error.message.includes("409"))
-            state.errCode = 409;
+            if(action.error.message.includes("409")) state.errCode = 409;
+        },
+        [loginUser.fulfilled] : (state, action) => {
+            localStorage.setItem("auth", action.payload.token)
+            state.errCode = null
+            state.token = action.payload.token
+        },
+        [loginUser.rejected] : (state, action) => {
+            if(action.error.message.includes("401")) state.errCode = 401; 
         }
     }
 })
