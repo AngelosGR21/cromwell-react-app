a9ff5cfdd33fb35fe511ec4ffe5b9cf98bd88f45
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-22 20:33:59 +0100

    adding data and images

---
+++ src/assets/dummyData.js
@@ -0,0 +1,49 @@
+import houseImg from "./house.jpg";
+import houseImg1 from "./house1.jpg";
+import houseImg2 from "./house2.jpg";
+import houseImg3 from "./house3.jpg";
+import houseImg4 from "./house4.jpg";
+import houseImg5 from "./house5.jpg";
+
+
+
+const data = [
+    {
+        id: 0,
+        img: houseImg,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    },
+    {
+        id: 1,
+        img: houseImg1,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    },
+    {
+        id: 2,
+        img: houseImg2,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    },
+    {
+        id: 3,
+        img: houseImg3,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    },
+    {
+        id: 4,
+        img: houseImg4,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    },
+    {
+        id: 5,
+        img: houseImg5,
+        title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue enim eget massa maximus, nec auctor neque tincidunt. Donec finibus sem ac arcu molestie, at interdum tortor pretium. Aliquam erat volutpat. Integer id convallis turpis. Phasellus hendrerit sem quis diam vehicula, vitae pulvinar odio blandit. Nulla suscipit lectus porta semper dictum."
+    }
+]
+
+export default data;
